10ccc2f04cf2d1e73155ed6af5d23874e876ed37
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2025-04-07 23:03:43 -0600

    Update: Add new font and packages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "@fontsource/im-fell-english/400.css";
+import "@fontsource/beth-ellen/400.css";
 
 import App from "@pages/App";
 import NotFound from "@pages/NotFound";
